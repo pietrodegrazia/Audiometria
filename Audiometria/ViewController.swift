d9f2795e80180beb71721191d237a5a5eec61832
Marcus Kuquert <marcus.kuquert@icloud.com>
2016-04-22 19:38:02 -0300

    Add carthage

--- Audiometria/ViewController.swift
+++ Audiometria/ViewController.swift
@@ -7,19 +7,31 @@
 //
 
 import UIKit
+import ResearchKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ORKTaskViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let myStep = ORKInstructionStep(identifier: "intro")
+        myStep.title = "Welcome to ResearchKit"
+        
+        let task = ORKOrderedTask(identifier: "task", steps: [myStep])
+        
+        let taskViewController = ORKTaskViewController(task: task, taskRunUUID: nil)
+        taskViewController.delegate = self
+        presentViewController(taskViewController, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        print(reason)
+    }
 
 }
 
