88e2fcd0b6f7f043f19943491d9c59d7cbc271b2
Marcus Kuquert <marcus.kuquert@icloud.com>
2016-04-25 15:59:03 -0300

    Add carthage, and a button to start a ORKToneAudiometryStep

--- Audiometria/ViewController.swift
+++ Audiometria/ViewController.swift
@@ -14,14 +14,23 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    @IBAction func button(sender: AnyObject) {
+        
         let myStep = ORKInstructionStep(identifier: "intro")
         myStep.title = "Welcome to ResearchKit"
         
-        let task = ORKOrderedTask(identifier: "task", steps: [myStep])
+        let audioStep = ORKToneAudiometryStep(identifier: "audimetry")
+        audioStep.title = "Pressione uma tecla para iniciar"
+        audioStep.toneDuration = 5
+        
+        let task = ORKOrderedTask(identifier: "task", steps: [myStep, audioStep])
         
         let taskViewController = ORKTaskViewController(task: task, taskRunUUID: nil)
         taskViewController.delegate = self
-        presentViewController(taskViewController, animated: true, completion: nil)
+        
+        self.presentViewController(taskViewController, animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +39,7 @@ class ViewController: UIViewController, ORKTaskViewControllerDelegate {
     }
     
     func taskViewController(taskViewController: ORKTaskViewController, didFinishWithReason reason: ORKTaskViewControllerFinishReason, error: NSError?) {
+        taskViewController.dismissViewControllerAnimated(true, completion: nil)
         print(reason)
     }
 
