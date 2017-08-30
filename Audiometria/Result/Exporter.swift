76f857017c2e46c37a362180029c670e178ffac4
Marcus Vinicius Kuquert <marcus.kuquert@icloud.com>
2017-08-30 00:01:48 -0300

    Add Exporter class

---
+++ Audiometria/Result/Exporter.swift
@@ -0,0 +1,95 @@
+//
+//  Exporter.swift
+//  Audiometria
+//
+//  Created by Marcus Vinicius Kuquert on 28/08/17.
+//  Copyright © 2017 darkshine. All rights reserved.
+//
+
+import Foundation
+import CSV
+
+enum ExporterError: Error {
+    case nilPatientResuls
+    case invalidDataObjectCasting
+}
+
+class Exporter {
+    
+    static func exportPatient(_ patient: Patient) throws -> Data {
+        
+        guard let resultsRealm = patient.results else {
+            throw ExporterError.nilPatientResuls
+        }
+        
+        let stream = OutputStream(toMemory: ())
+        let csv = try CSVWriter(stream: stream)
+        
+        // Write CSV Header
+        try csv.write(row: ["patient",
+                            "freq500_20db",
+                            "freq500_40db",
+                            "freq500_60db",
+                            
+                            "freq1000_20db",
+                            "freq1000_40db",
+                            "freq1000_60db",
+                            
+                            "freq2000_20db",
+                            "freq2000_40db",
+                            "freq2000_60db",
+                            
+                            "freq4000_20db",
+                            "freq4000_40db",
+                            "freq4000_60db",
+                            
+                            "freq8000_20db",
+                            "freq8000_40db",
+                            "freq8000_60db"])
+        
+        
+        let id = patient.id
+        let freq500_20db = String(resultsRealm.freq500_20db)
+        let freq500_40db = String(resultsRealm.freq500_40db)
+        let freq500_60db = String(resultsRealm.freq500_60db)
+        let freq1000_20db = String(resultsRealm.freq1000_20db)
+        let freq1000_40db = String(resultsRealm.freq1000_40db)
+        let freq1000_60db = String(resultsRealm.freq1000_60db)
+        let freq2000_20db = String(resultsRealm.freq2000_20db)
+        let freq2000_40db = String(resultsRealm.freq2000_40db)
+        let freq2000_60db = String(resultsRealm.freq2000_60db)
+        let freq4000_20db = String(resultsRealm.freq4000_20db)
+        let freq4000_40db = String(resultsRealm.freq4000_40db)
+        let freq4000_60db = String(resultsRealm.freq4000_60db)
+        let freq8000_20db = String(resultsRealm.freq8000_20db)
+        let freq8000_40db = String(resultsRealm.freq8000_40db)
+        let freq8000_60db = String(resultsRealm.freq8000_60db)
+        
+        let r = [id,
+                 freq500_20db,
+                 freq500_40db,
+                 freq500_60db,
+                 freq1000_20db,
+                 freq1000_40db,
+                 freq1000_60db,
+                 freq2000_20db,
+                 freq2000_40db,
+                 freq2000_60db,
+                 freq4000_20db,
+                 freq4000_40db,
+                 freq4000_60db,
+                 freq8000_20db,
+                 freq8000_40db,
+                 freq8000_60db]
+        
+        try csv.write(row: r)
+        
+        csv.stream.close()
+        
+        if let csvData = stream.property(forKey: .dataWrittenToMemoryStreamKey) as? NSData {
+            return Data(referencing: csvData)
+        } else {
+            throw ExporterError.invalidDataObjectCasting
+        }
+    }
+}
