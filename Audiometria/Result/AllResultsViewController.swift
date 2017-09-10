82c803d2a1260b3e5767bb16f9dc2e0b85e74404
Marcus Vinicius Kuquert <marcus.kuquert@icloud.com>
2017-09-10 17:19:33 -0300

    Update AllResultsVC to share results.

--- Audiometria/Result/AllResultsViewController.swift
+++ Audiometria/Result/AllResultsViewController.swift
@@ -7,35 +7,56 @@
 //
 
 import UIKit
+import RealmSwift
+import MessageUI
 
-class AllResultsViewController: UIViewController {
+class AllResultsViewController: UIViewController, SimpleEmailComposer {
     
-    //The key is the patient ID
+    @IBOutlet weak var tableView: UITableView!
+    
     var patients = [Patient]()
+    
+    private lazy var shareBarButtonItem: UIBarButtonItem = { () -> UIBarButtonItem in
+        let button = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.action,
+                                     target: self,
+                                     action: #selector(shareButtonAction))
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.action,
-                                                 target: self,
-                                                 action: #selector(shareButtonAction))
+        self.navigationItem.rightBarButtonItem = shareBarButtonItem
         
-        self.navigationItem.rightBarButtonItem = rightBarButtonItem
+        
+        retrivePatientsFromRealm()
+        tableView.reloadData()
+        
     }
     
-    func shareButtonAction() {
-        //TODO: Implements export sheet, including e-mail export
+    private func retrivePatientsFromRealm() {
+        do {
+            patients = try Array(Realm().objects(Patient.self))
+            patients.sort { $0.0.timestamp > $0.1.timestamp }
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    @objc private func shareButtonAction() {
+        let data = try! Exporter.exportPatients(patients)
+        sendEmailWithData(data, fileName: "allPatients.csv")
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == Segue.ShowResult.rawValue,
-            let results = sender as? Results,
+            let patient = sender as? Patient,
             let vc = segue.destination as? ResultViewController {
-            vc.results = results
+            vc.patient = patient
+            
         }
     }
 }
-
 
 extension AllResultsViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -49,16 +70,35 @@ extension AllResultsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PatientCell", for: indexPath)
         let patient = patients[indexPath.row]
-        cell.textLabel?.text = patient.id
+        let boldFont = UIFont.boldSystemFont(ofSize: 16)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/YY - HH:mm:ss"
+        
+        cell.textLabel?.text = patient.reabableId() + " (" + dateFormatter.string(from: patient.timestamp) + ")"
+        cell.textLabel?.font = boldFont
+        
+        cell.textLabel?.textColor = UIColor(red: 0x99/255,
+                                            green: 0x99/255,
+                                            blue: 0x9A/255,
+                                            alpha: 1.0)
         return cell
     }
 }
 
 extension AllResultsViewController: UITableViewDelegate {
-    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let patient = patients[indexPath.row]
-        performSegue(withIdentifier: Segue.ShowResult.rawValue, sender: patient.result)
+        performSegue(withIdentifier: Segue.ShowResult.rawValue, sender: patient)
     }
     
+}
+
+extension AllResultsViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        if error != nil {
+            print("Error: \(String(describing: error))")
+        }
+        dismiss(animated: true)
+    }
 }
